97a59c02a648495611ac62096a4a7cefa286fd8b
Andrew <44811609+Andryshatina@users.noreply.github.com>
2022-08-14 14:27:02 +0300

    added bot menu with filtering and some improvments

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import uniqid from 'uniqid';
 import './App.css';
 import TaskList from './Components/TaskList/TaskList';
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
   const [input, setInput] = useState('');
+
   const [taskList, setTaskList] = useState([
     {
       id: uniqid(),
@@ -12,6 +14,22 @@ const App = () => {
       isDone: false
     }
   ]);
+
+  const [filter, setFilter] = useState('all');
+
+  const onFilter = () => {
+    switch (filter) {
+      case 'all':
+        return taskList;
+      case 'active':
+        return taskList.filter(task => !task.isDone);
+      case 'completed':
+        return taskList.filter(task => task.isDone);
+      default:
+        return taskList;
+    }
+  };
+
 
   const onInputChange = (e) => {
     setInput(e.target.value);
@@ -30,19 +48,27 @@ const App = () => {
     setTaskList(temp);
   }
 
+  const deleteCompleted = () => {
+    setTaskList(taskList.filter(task => !task.isDone));
+  }
+
   const toggleDone = (idx) => {
     let temp = [...taskList];
     temp[idx].isDone = !temp[idx].isDone;
     setTaskList(temp);
   }
 
-
+  let filteredTaskList = onFilter()
   return (
+
     <div className="App">
+      <h1>todos</h1>
       <div className='AddTask' >
-        <input placeholder='Add a new task' value={input} onChange={(e) => onInputChange(e)} onKeyDown={(e) => addTask(e)} />
+        <input placeholder='What needs to be done?' value={input} onChange={(e) => onInputChange(e)} onKeyDown={(e) => addTask(e)} />
       </div>
-      <TaskList taskList={taskList} toggleDone={toggleDone} deleteTask={deleteTask} />
+      <TaskList taskList={filteredTaskList} toggleDone={toggleDone} deleteTask={deleteTask} />
+      <Footer taskList={taskList} deleteCompleted={deleteCompleted} setFilter={setFilter} />
+      {console.log(filter)}
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+
+const Footer = ({ taskList, deleteCompleted, setFilter }) => {
+
+	return (
+		<div>
+			<p>{taskList.filter(task => !task.isDone).length} tasks left</p>
+			<div>
+				<button onClick={() => setFilter('all')}>All</button>
+				<button onClick={() => setFilter('active')}>Active</button>
+				<button onClick={() => setFilter('completed')}>Completed</button>
+			</div>
+			<button onClick={deleteCompleted}>Clear Completed</button>
+		</div>
+	)
+}
+
+export default Footer;
